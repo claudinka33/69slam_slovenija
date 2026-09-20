1b9eb1ae02cd567a3f7763a8ac177a6b81a68fa5
claudinka33 <claudinka33@gmail.com>
2026-09-20 10:57:36 +0200

    Footer: podatki podjetja + povezave na pravne strani
    Updated footer links to use dynamic company information and added additional legal information.

--- app/[lang]/layout.js
+++ app/[lang]/layout.js
@@ -1,6 +1,7 @@
 import { notFound } from "next/navigation";
 import { getProducts } from "../../lib/catalog";
 import { getDict, LANGS } from "../../lib/i18n";
+import { COMPANY } from "../../lib/legal";
 import { CartProvider } from "../../components/CartContext";
 import Header from "../../components/Header";
 import CartDrawer from "../../components/CartDrawer";
@@ -33,17 +34,25 @@ export default async function LangLayout({ children, params }) {
           </div>
           <div>
             <h4>{t.foot_help}</h4>
-            <a href="#">{t.f_ship}</a>
-            <a href="#">{t.f_return}</a>
-            <a href="#">{t.f_terms}</a>
+            <a href={`/${lang}/info/dostava-in-placilo`}>{t.f_ship}</a>
+            <a href={`/${lang}/info/vracila-in-odstop`}>{t.f_return}</a>
+            <a href={`/${lang}/info/reklamacije`}>{t.f_claims}</a>
+            <a href={`/${lang}/info/splosni-pogoji`}>{t.f_terms}</a>
+            <a href={`/${lang}/info/zasebnost`}>{t.f_privacy}</a>
+            <a href={`/${lang}/info/piskotki`}>{t.f_cookies}</a>
           </div>
           <div>
             <h4>{t.foot_contact}</h4>
-            <a href="mailto:info@69slam.si">info@69slam.si</a>
+            <a href={`mailto:${COMPANY.email}`}>{COMPANY.email}</a>
+            <a href={`tel:+386${COMPANY.phone.replace(/\s/g, "").slice(1)}`}>{COMPANY.phone}</a>
             <a href="#">Instagram</a>
             <a href="#">Facebook</a>
           </div>
-          <div className="legal">{t.legal}</div>
+          <div className="legal">
+            {COMPANY.name} · {COMPANY.address} · Matična št.: {COMPANY.reg} · ID za DDV: {COMPANY.vat} · Vpis: {COMPANY.court} · Osnovni kapital: {COMPANY.capital}
+            <br />
+            {t.legal}
+          </div>
         </div>
       </footer>
       <CartDrawer lang={lang} t={t} />
